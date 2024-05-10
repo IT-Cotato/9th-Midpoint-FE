4ec15100748dbb3c861b56c90a0c12ddff0d18dd
Cllaude99 <xodbsrla1025@naver.com>
2024-05-11 03:19:06 +0900

    test: 경로설정 테스트 코드

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <div>
         <h1>CI / CD 구축 완료 및 배포자동화완료!</h1>
-        <p>연동 2차 테스트 + DNS 변경사항 확인용</p>
+        <p>경로설정 테스트</p>
       </div>
       <Title>Home</Title>
       <HomeSvg />
