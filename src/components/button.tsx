d791626203921c6fd6aac1347d0777ef6c130516
Cllaude99 <xodbsrla1025@naver.com>
2024-05-19 22:51:22 +0900

    feat: 사용자 중간지점 입력 및 api연동 코드 작성

---
+++ src/components/button.tsx
@@ -0,0 +1,16 @@
+interface IButton {
+  text: string;
+  isLoading: boolean;
+}
+
+export default function Button({ text, isLoading }: IButton) {
+  return (
+    <button
+      type="submit"
+      className="h-10 primary-btn disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
+      disabled={isLoading}
+    >
+      {isLoading ? '잠시만 기다려 주세요...' : text}
+    </button>
+  );
+}
