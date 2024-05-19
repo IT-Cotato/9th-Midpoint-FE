3cd1f846c8591fd937c903d3c1e092f792b50573
Cllaude99 <xodbsrla1025@naver.com>
2024-05-19 22:50:30 +0900

    feat: kakao api연동 및 마커 생성

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -2,15 +2,7 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    red: string;
-    black: {
-      veryDark: string;
-      darker: string;
-      lighter: string;
-    };
-    white: {
-      lighter: string;
-      darker: string;
-    };
+    lightPurple: string;
+    mainColor: string;
   }
 }
