a420924d64bbf00580d011a09508f856a7e4f365
Cllaude99 <xodbsrla1025@naver.com>
2024-08-16 22:41:14 +0900

    feat: 장소투표 구현

--- src/pages/PlaceVote/Result/PlaceResultAlone.tsx
+++ src/pages/PlaceVote/Result/PlaceResultAlone.tsx
@@ -60,34 +60,33 @@ export default function PlaceResultAlone() {
         <h1 className="text-2xl font-semibold text-[#1A3C95]">이번 모임 장소는...</h1>
         <p className="text-gray-600 text-md">이번 모임 만남이 가능한 장소를 확인해보세요!</p>
       </div>
-      <div>
+      <div className="flex flex-col gap-3">
         {placeVoteResult.map((place) => (
-          <div key={place.id} className="grid grid-cols-2 gap-3 p-3">
+          <div key={place.id} className="grid items-center grid-cols-2 gap-3 p-3">
             <span className="w-1/5 p-2 text-xl font-semibold text-center text-blue-500 bg-white rounded-md shadow-md justify-self-center">
               {place.count}표
             </span>
-            <span className="w-4/5 p-2 px-4 text-xl font-medium bg-white rounded-md shadow-md">
+            <span className="px-4 py-2 overflow-x-auto text-xl font-semibold text-center bg-white rounded-md shadow-md min-w-2/5">
               {place.roadNameAddress}
             </span>
           </div>
         ))}
-      </div>
-
-      <div className="flex justify-center gap-5 mt-6">
-        <button
-          type="button"
-          className="w-2/5 py-3 font-semibold text-white transition-colors rounded-lg min-h-10 primary-btn"
-          onClick={() => navigate(`/page/a/place-vote/${roomId}`)}
-        >
-          투표 다시하기
-        </button>
-        <button
-          type="button"
-          className="w-2/5 py-3 font-semibold text-gray-600 transition-colors bg-gray-300 rounded-lg hover:bg-gray-400"
-          onClick={() => navigate(`/page/a/create/place-vote-room/${roomId}`)}
-        >
-          투표 재생성하기
-        </button>
+        <div className="flex justify-center w-full gap-3 p-3">
+          <button
+            type="button"
+            className="w-1/2 py-3 font-semibold text-white transition-colors rounded-lg min-h-10 primary-btn"
+            onClick={() => navigate(`/page/a/place-vote/${roomId}`)}
+          >
+            투표 다시하기
+          </button>
+          <button
+            type="button"
+            className="w-1/2 py-3 font-semibold text-gray-600 transition-colors bg-gray-300 rounded-lg hover:bg-gray-400"
+            onClick={() => navigate(`/page/a/create/place-vote-room/${roomId}`)}
+          >
+            투표 재생성하기
+          </button>
+        </div>
       </div>
     </div>
   );
