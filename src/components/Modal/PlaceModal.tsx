2060870d03ac0f980bdd0855f7001f0042aedaec
Cllaude99 <xodbsrla1025@naver.com>
2024-08-21 03:20:51 +0900

    fix: 경로 최종 수정

--- src/components/Modal/PlaceModal.tsx
+++ src/components/Modal/PlaceModal.tsx
@@ -46,15 +46,25 @@ export default function PlaceModal({ isOpen, onClose }: ModalProps) {
     localStorage.removeItem('refreshToken');
     localStorage.removeItem('roomId');
     if (selectedOption === 'friend') {
-      const { data } = await axios.post(BACKEND_URL + '/api/rooms', {
-        roomType: ROOM_TYPE_EACH,
-      });
+      const { data } = await axios.post(
+        BACKEND_URL + '/api/rooms',
+        {
+          roomType: ROOM_TYPE_EACH,
+        },
+        { withCredentials: true },
+      );
       setRoomId(data.data.id);
       setCurrentView('shareLink');
     } else {
-      const { data } = await axios.post(BACKEND_URL + '/api/rooms', {
-        roomType: ROOM_TYPE_ALONE,
-      });
+      const { data } = await axios.post(
+        BACKEND_URL + '/api/rooms',
+        {
+          roomType: ROOM_TYPE_ALONE,
+        },
+        {
+          withCredentials: true,
+        },
+      );
       const newRoomId = data.data.id;
       navigate(`/page/a/results/${newRoomId}`);
     }
