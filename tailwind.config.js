aea1d4c055bbd994a55c0fc5ac92d206f2c0db71
Cllaude99 <xodbsrla1025@naver.com>
2024-10-20 17:35:38 +0900

    style: 디자인 시스템 구축

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        suit: 'SUIT',
+        pre: 'Pretendard',
+      },
+    },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    function ({ addBase, addComponents }) {
+      addBase({
+        body: {
+          overflowX: 'hidden',
+        },
+        html: {
+          fontFamily: "'SUIT', 'Pretendard', sans-serif",
+        },
+      });
+
+      addComponents({
+        '.primary-btn': {
+          width: '100%',
+          backgroundColor: '#5786FF',
+          color: '#ffffff',
+          fontWeight: '500',
+          borderRadius: '0.5rem',
+          textAlign: 'center',
+          '&:hover': {
+            backgroundColor: '#60A5FA',
+          },
+          transitionProperty: 'colors',
+        },
+      });
+    },
+  ],
 };
