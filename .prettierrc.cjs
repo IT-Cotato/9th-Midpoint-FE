3b6dc0aa41548636e8c366d7474a14283712a3ae
Cllaude99 <xodbsrla1025@naver.com>
2024-10-20 17:33:29 +0900

    remove: 의미없는 파일 삭제

--- .prettierrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: "all",
-  singleQuote: true,
-  semi: true,
-  useTabs: false,
-  tabWidth: 2,
-  printWidth: 120,
-};
--- src/styles/shared/theme.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultTheme } from 'styled-components';
-
-export const theme: DefaultTheme = {
-  bgColor: '#EFF3FF',
-  mainColor: '#5786FF',
-};
