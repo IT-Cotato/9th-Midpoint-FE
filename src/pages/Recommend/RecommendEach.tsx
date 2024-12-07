81307cf31a964e6bac052a06f161c01b412a2642
“chae-dahee” <cdh010126r@gmail.com>
2024-12-07 16:21:18 +0900

    refactor: 장소투표 폴더구조. vote, result 는 api를 쿼리로 변경해야함. - 주석파일 확인 후 삭제

--- src/pages/Recommend/RecommendEach.tsx
+++ src/pages/Recommend/RecommendEach.tsx
@@ -1,7 +1,19 @@
-import { axiosInstance, BACKEND_URL } from '@/apis';
-import { FROM_EACH_RESULT, PLACE_ALL, PLACE_CAFE, PLACE_RESTAURANT, PLACE_STUDY, ROOM_TYPE_EACH } from '@/constants';
+import { axiosInstance, BACKEND_URL } from '@/apis/index.api';
+import {
+  FROM_EACH_RESULT,
+  PLACE_ALL,
+  PLACE_CAFE,
+  PLACE_RESTAURANT,
+  PLACE_STUDY,
+  ROOM_TYPE_EACH,
+} from '@/constants';
 import { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import {
+  Link,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from 'react-router-dom';
 import Loading from '../Loading/loading';
 import BooksIcon from '@/assets/imgs/Midpoints/books.svg?react';
 import CafeIcon from '@/assets/imgs/Midpoints/cafe.svg?react';
@@ -35,7 +47,8 @@ export default function RecommendEach() {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const [coordinates, setCoordinates] = useState<Coordinate[]>([]);
-  const [selectedCoordinate, setSelectedCoordinate] = useState<Coordinate | null>(null);
+  const [selectedCoordinate, setSelectedCoordinate] =
+    useState<Coordinate | null>(null);
   const [clickedItemNum, setClickedItemNum] = useState<number | null>(null);
   const [currentPage, setCurrentPage] = useState<number>(0);
   const [totalPages, setTotalPages] = useState<number | null>(null);
@@ -55,28 +68,33 @@ export default function RecommendEach() {
 
     async function fetchRecommendations(page: number) {
       try {
-        const { data } = await axiosInstance.get(`${BACKEND_URL}/api/recommend-places`, {
-          headers: {
-            RoomId: roomId!,
-            RoomType: ROOM_TYPE_EACH,
+        const { data } = await axiosInstance.get(
+          `${BACKEND_URL}/api/recommend-places`,
+          {
+            headers: {
+              RoomId: roomId!,
+              RoomType: ROOM_TYPE_EACH,
+            },
+            params: {
+              addressLat: lat,
+              addressLong: lng,
+              placeStandard: clickedOption,
+              page: page,
+            },
           },
-          params: {
-            addressLat: lat,
-            addressLong: lng,
-            placeStandard: clickedOption,
-            page: page,
-          },
-        });
+        );
         setClickedItemNum(0);
         setRecommendations(data.data.content);
         setTotalPages(data.data.totalPages);
         if (data.data.content) {
           setCoordinates(
-            data.data.content.map(({ name, addressLat, addressLong }: IContents) => ({
-              lat: addressLat,
-              lng: addressLong,
-              name,
-            })),
+            data.data.content.map(
+              ({ name, addressLat, addressLong }: IContents) => ({
+                lat: addressLat,
+                lng: addressLong,
+                name,
+              }),
+            ),
           );
           setSelectedCoordinate({
             lat: data.data.content[0].addressLat,
@@ -87,7 +105,9 @@ export default function RecommendEach() {
         setLoading(false);
       } catch (error: any) {
         if (error.response && error.response.status === 401) {
-          navigate(`/page/login/${roomId}`, { state: { from: FROM_EACH_RESULT } });
+          navigate(`/page/login/${roomId}`, {
+            state: { from: FROM_EACH_RESULT },
+          });
         }
         if (error.response && error.response.status === 403) {
           navigate(`/page/each/${roomId}`);
@@ -106,7 +126,12 @@ export default function RecommendEach() {
     setCurrentPage(1);
   }, [clickedOption]);
 
-  function handleClickItem(name: string, addressLat: number, addressLong: number, index: number) {
+  function handleClickItem(
+    name: string,
+    addressLat: number,
+    addressLong: number,
+    index: number,
+  ) {
     setClickedItemNum(index);
     setSelectedCoordinate({ lat: addressLat, lng: addressLong, name });
   }
@@ -149,7 +174,9 @@ export default function RecommendEach() {
   return (
     <div className="flex flex-col w-4/5 gap-3">
       <div className="flex items-center w-full">
-        <span className="w-1/2 font-semibold text-[#2F5FDD] text-lg">{name}에서 뭐하지?</span>
+        <span className="w-1/2 font-semibold text-[#2F5FDD] text-lg">
+          {name}에서 뭐하지?
+        </span>
         <ul className="flex w-1/2 gap-2 *:font-semibold *:cursor-pointer *:rounded-3xl *:py-[6px] *:px-4 *:w-max *:shadow-md ">
           <li
             onClick={() => handleClickedOption(PLACE_ALL)}
@@ -181,20 +208,37 @@ export default function RecommendEach() {
       </div>
       <div className="grid grid-cols-2 w-full gap-3 grid-rows-[auto_1fr]">
         <div className="h-[700px] rounded-2xl shadow-lg">
-          <MidpointMap coordinates={coordinates} selectedCoordinate={selectedCoordinate} />
+          <MidpointMap
+            coordinates={coordinates}
+            selectedCoordinate={selectedCoordinate}
+          />
         </div>
         <div className="flex flex-col gap-3">
           {recommendations.map(
             (
-              { name, siDo, siGunGu, roadNameAddress, placeUrl, phoneNumber, addressLat, addressLong, placeStandard },
+              {
+                name,
+                siDo,
+                siGunGu,
+                roadNameAddress,
+                placeUrl,
+                phoneNumber,
+                addressLat,
+                addressLong,
+                placeStandard,
+              },
               index,
             ) => (
               <div
                 key={index}
                 className={`bg-[#F8F8FB] rounded-2xl cursor-pointer flex flex-col p-3 pl-6 gap-1 transition-all ${
-                  clickedItemNum === index ? 'bg-blue-100 opacity-95 ring-2 ring-blue-500' : ''
+                  clickedItemNum === index
+                    ? 'bg-blue-100 opacity-95 ring-2 ring-blue-500'
+                    : ''
                 } h-max`}
-                onClick={() => handleClickItem(name, addressLat, addressLong, index)}
+                onClick={() =>
+                  handleClickItem(name, addressLat, addressLong, index)
+                }
               >
                 <div className="*:font-semibold *:text-[#1A3C95] pt-1">
                   {placeStandard === PLACE_STUDY && (
@@ -217,7 +261,9 @@ export default function RecommendEach() {
                   )}
                 </div>
                 <div className="flex items-center gap-2 ">
-                  <h1 className="text-xl text-[#2F5FDD] font-semibold">{name}</h1>
+                  <h1 className="text-xl text-[#2F5FDD] font-semibold">
+                    {name}
+                  </h1>
                   <span>
                     <Link to={placeUrl} target="_blank">
                       <LinkIcon
@@ -230,23 +276,39 @@ export default function RecommendEach() {
                   <span className="text-[#5E6D93]">
                     {siDo} {siGunGu} {roadNameAddress}
                   </span>
-                  <span className="text-[#2F5FDD] bg-[#EFF3FF] font-semibold p-1 rounded-lg">{phoneNumber}</span>
+                  <span className="text-[#2F5FDD] bg-[#EFF3FF] font-semibold p-1 rounded-lg">
+                    {phoneNumber}
+                  </span>
                 </div>
               </div>
             ),
           )}
           <div className="flex items-center justify-center gap-2 mt-4">
             {currentPage > 5 && (
-              <button onClick={() => handlePageClick(Math.floor((currentPage - 1) / 5) * 5)}>
+              <button
+                onClick={() =>
+                  handlePageClick(Math.floor((currentPage - 1) / 5) * 5)
+                }
+              >
                 <PrevBtn />
               </button>
             )}
             {renderPageNumbers()}
-            {currentPage < totalPages! && Math.floor((currentPage - 1) / 5) * 5 + 5 < totalPages! && (
-              <button onClick={() => handlePageClick(Math.min(Math.floor((currentPage - 1) / 5) * 5 + 6, totalPages!))}>
-                <NextBtn />
-              </button>
-            )}
+            {currentPage < totalPages! &&
+              Math.floor((currentPage - 1) / 5) * 5 + 5 < totalPages! && (
+                <button
+                  onClick={() =>
+                    handlePageClick(
+                      Math.min(
+                        Math.floor((currentPage - 1) / 5) * 5 + 6,
+                        totalPages!,
+                      ),
+                    )
+                  }
+                >
+                  <NextBtn />
+                </button>
+              )}
           </div>
         </div>
       </div>
